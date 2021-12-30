806c0e295bef6d981187f8f1660d99d539b329af
Khin Htet Htet Oo <khinhtet.km@gmail.com>
2021-12-30 15:12:41 +0630

    team

---
+++ js/app.js
@@ -0,0 +1,11 @@
+//UI
+
+const banner = document.querySelector('.banners');
+const blocks = document.getElementsByClassName('blocks');
+
+  
+for (var i = 1; i < 400; i++) {
+	banner.innerHTML += "<div class='blocks'></div>";
+	blocks[i].style.animationDelay = `${i * 0.05}s`;
+	// blocks[i].style.animationDelay = `${i * 0.05}s`;
+}
